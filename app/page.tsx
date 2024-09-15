f1c76a555396dfd2105f31e5d5415bfb344b2ae2
omarwagih <omarwagih95@gmail.com>
2024-09-15 16:59:38 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,50 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
+  // const [hovered,setHovered] =useState(false);
+  const [xAxis,setX] =useState(55);
+  const [yAxis,setY] =useState(75);
+  const handleHover =() =>{
+    console.log('gowa alfunc')
+    const x= Math.floor(Math.random()*90)
+    const y= Math.floor(Math.random()*90)
+      setX(x);
+      setY(y);
+      console.log(xAxis , yAxis)
+  }
+// const handleMouseEnter = (): void => {
+//   console.log('here')
+//   setHovered((prev)=>prev!);
+//   console.log(hovered)
+//   // setX(event.clientX - 100);
+//   // setY(event.clientY - 100);
+// };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+<div className="h-screen w-screen flex gap-8 bg-pink-50 text-gray-800 justify-center items-center flex-col relative">
+  <Image alt="dobba" height={350} width={280} src={'/dobbaQues.gif'}></Image>
+  <h1 className="font-bold text-2xl">DO YOU LOVE ME ?</h1>
+  <div className="flex gap-5">
+    <div className="p-8">
+    <Link href={'/yes'} >
+    
+    <button 
+    className="bg-blue-600 absolute left-[40vw] top-[75vh]  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">yes</button>
+    </Link>
     </div>
+    <div onMouseEnter={handleHover}
+    
+ className="p-8">
+<button
+  style={{ left: `${xAxis}vw`, top: `${yAxis}vh` }} 
+  className={`bg-red-500  absolute  text-white font-bold py-2 px-4 rounded-md`}
+>
+  no
+</button>
+    </div>
+  </div>
+</div>
   );
 }
---
+++ app/yes/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+    <div className='flex flex-col w-screen bg-pink-50 text-gray-800 h-screen justify-center items-center'>
+          <Image alt="dobba" height={350} width={280} src={'/spin.gif'}></Image>
+          <h1 className="font-bold text-2xl">Habeeeebyyyyyyyy </h1>
+
+    
+    </div>
+  )
+}
+
+export default page
