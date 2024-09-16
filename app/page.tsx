c1bb3ea0dd7f0fbef98d8c187bff13244f690d36
omarwagih <omarwagih95@gmail.com>
2024-09-16 18:00:49 +0300

    mob responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
@@ -13,15 +13,29 @@ export default function Home() {
     const y= Math.floor(Math.random()*90)
       setX(x);
       setY(y);
-      console.log(xAxis , yAxis)
   }
-// const handleMouseEnter = (): void => {
-//   console.log('here')
-//   setHovered((prev)=>prev!);
-//   console.log(hovered)
-//   // setX(event.clientX - 100);
-//   // setY(event.clientY - 100);
-// };
+  // useEffect(() => {
+  //   console.log(xAxis, yAxis);
+  // }, [xAxis, yAxis]);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        // Small screen sizes (mobile)
+        setX(60);
+        setY(75);
+      } else {
+        // Larger screen sizes (desktop/tablet)
+        setX(55);
+        setY(75);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Run on mount to set initial values
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
 <div className="h-screen w-screen flex gap-8 bg-pink-50 text-gray-800 justify-center items-center flex-col relative">
   <Image alt="dobba" height={350} width={280} src={'/dobbaQues.gif'}></Image>
@@ -29,19 +43,16 @@ export default function Home() {
   <div className="flex gap-5">
     <div className="p-8">
     <Link href={'/yes'} >
-    
     <button 
-    className="bg-blue-600 absolute left-[40vw] top-[75vh]  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">yes</button>
+    className="bg-blue-600 absolute left-[30vw] sm:left-[40vw] top-[75vh]  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">yes</button>
     </Link>
     </div>
     <div onMouseEnter={handleHover}
-    
- className="p-8">
+    className="p-8">
 <button
 onClick={handleHover}
-  style={{ left: `${xAxis}vw`, top: `${yAxis}vh` }} 
-  className={`bg-red-500  absolute  text-white font-bold py-2 px-4 rounded-md`}
->
+  style={{ left: ` ${xAxis}vw`, top: ` ${yAxis}vh` }} 
+  className={`bg-red-500  absolute  text-white font-bold py-2 px-4 rounded-md`}>
   no
 </button>
     </div>
